2702be23d796b5911310cda13241123d16f0673d
Michael Dera <michael@devshop.co.zw>
2018-12-09 22:24:23 +0200

    added Page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Page from './components/Page/Page';
 
 export default class App extends React.Component <any, any> {
   public render() {
     return (
       <div className="App">
         <Header/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Page/>
       </div>
     );
   }
---
+++ src/components/Page/Page.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default class Page extends React.Component <any, any> {
+  public render() {
+    return (
+      <div className="page">
+      </div>
+    );
+  }
+}
