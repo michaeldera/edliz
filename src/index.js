620a37f95fce6eaa53ad9666afa87f111efd3345
Michael Dera <michael@devshop.co.zw>
2018-12-15 09:50:53 +0200

    added twitter cards data and facebook opengraph

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Fabric><App /></Fabric>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
