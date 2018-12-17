c4e9a358f109713049c056e311c2c6d96ba7f34d
Michael Dera <michael@devshop.co.zw>
2018-12-17 11:50:48 +0200

    added a Home Component

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+          <h1>Essential Drugs and Medicines List</h1>
+          <p>Home page</p> 
+      </div>
+    );
+  }
+}
