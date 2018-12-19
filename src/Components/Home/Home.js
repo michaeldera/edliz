219671b12216a8cc292d9cfd0795f0ec4300bce3
Michael Dera <michael@devshop.co.zw>
2018-12-19 08:15:19 +0200

    added style

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './home.css';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className="home-page">
           <h1>Essential Drugs and Medicines List</h1>
           <p>Home page</p> 
       </div>
