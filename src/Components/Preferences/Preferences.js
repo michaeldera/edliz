0b6cfb18f34f7dee12eacd90c4c7827f0a0a5289
Michael Dera <michael@devshop.co.zw>
2018-12-20 14:37:28 +0200

    minor bug fix and refactor

--- src/Components/Preferences/Preferences.js
+++ src/Components/Preferences/Preferences.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import './preferences.css'
 
 
-class Preferences extends Component {
+ export default class Preferences extends Component {
     render() {
         const bottom = (this.props.status === "open")? "0": "-290px";
         return (
             <div className="preferences" style={{ bottom : bottom }} >                
-                <label for="font-size-input" className="font-size-label">Font-size: <span>{this.props.fontSize}pt</span></label>
+                <label htmlFor="font-size-input" className="font-size-label">font-size</label>
                 <input name="font-size-input" className="font-size-input" type="range" min="8" max="30" placeholder={this.props.fontSize} onChange={this.props.handleFontSizeChange()}/>
                 <div className="preference-control">
                     <button className="btn-mode" onClick={this.props.toggleMode()}>{(this.props.mode === "day" )? "night mode" :  "day mode"}</button>
@@ -17,5 +17,3 @@ class Preferences extends Component {
         );
     }
 }
-
-export default Preferences;
