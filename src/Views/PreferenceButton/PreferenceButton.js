6991b861dff5862cc69302484513cf25b8276ea0
Michael Dera <michael@devshop.co.zw>
2018-12-20 06:26:21 +0200

    changed icon on button

--- src/Views/PreferenceButton/PreferenceButton.js
+++ src/Views/PreferenceButton/PreferenceButton.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import settings from './../../Assets/Icons/settings.svg';
+import icon from './cog.svg';
 import './preferencebtn.css'
 
 class PreferenceButton extends Component {
@@ -7,7 +7,7 @@ class PreferenceButton extends Component {
         return(
             <div>
                 <button className="nav-btn" onClick={this.props.toggle()}>
-                    <img src={settings} alt="Navigate"/>
+                    <img src={icon} alt="Navigate"/>
                 </button>
             </div>    
         );
