47d47cf3a09e204c89a2fc5cd3498cb558cec184
Michael Dera <michael@devshop.co.zw>
2018-12-27 08:42:23 +0200

    bug fix: font size input

--- src/Components/Preferences/Preferences.js
+++ src/Components/Preferences/Preferences.js
@@ -8,7 +8,7 @@ import './preferences.css'
         return (
             <div className="preferences" style={{ bottom : bottom }} >                
                 <label htmlFor="font-size-input" className="font-size-label">font-size</label>
-                <input name="font-size-input" className="font-size-input" type="range" min="8" max="30" placeholder={this.props.fontSize} onChange={this.props.handleFontSizeChange()}/>
+                <input name="font-size-input" className="font-size-input" type="range" min="8" max="30" placeholder={11} onChange={this.props.handleFontSizeChange()}/>
                 <div className="preference-control">
                     <button className="btn-mode" onClick={this.props.toggleMode()}>{(this.props.mode === "day" )? "night mode" :  "day mode"}</button>
                 </div>
