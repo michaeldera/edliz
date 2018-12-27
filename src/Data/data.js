1cc6fdfb125a35818377661940432cb1370f39a6
josephx86 <simplex86@gmail.com>
2018-12-27 02:57:58 -0600

    Edition correction in foreword
    Changed the edition from 15th to 7th. It is most likely '15' is the year the current edition was published but the edition itself if the 7th.

--- src/Data/data.js
+++ src/Data/data.js
@@ -2,10 +2,10 @@
 import React from 'react';
 const foreword = {
   short_title: "Foreword",
-  long_title: "Foreword on the 15th Edition",
+  long_title: "Foreword on the 7th Edition",
   content:
     <span>
-      <h1>Foreword on the 15th Edition</h1>
+      <h1>Foreword on the 7th Edition</h1>
       <p>It is the national objective that the health care needs of Zimbabweans are met through the provision and proper use of essential medicines. Sometimes we do not need to give medicines, that is, there is not always a - "pill for every ill".  Thus, there is need to use medicines appropriately, efficiently, and effectively.</p>
       <p>The guidelines in EDLIZ have always reflected the consensus of local experts, and takes into consideration factors such as the Zimbabwean setting, prevailing economic climate, practical experience as well as evidence-based therapeutics.</p>
       <p>This new EDLIZ has taken into account the dynamic changes in the Burden of Disease as reflected by the inclusion of antiretroviral medicines and treatment of other opportunistic infections other than Tuberculosis (TB). Many of the therapeutic regimens of the previous EDLIZ still hold true and remain the same, and should reinforce the confidence of the prescriber in making reliable therapeutic choices.</p>
