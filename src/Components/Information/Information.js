ca2a07f121ece0d08f0f08e45355dc30dfd9a53c
Michael Dera <michael@devshop.co.zw>
2018-12-29 08:49:30 +0200

    scroll to top improvement

--- src/Components/Information/Information.js
+++ src/Components/Information/Information.js
@@ -6,7 +6,7 @@ import { bookContents } from '../../Data/data'
 export default class Information extends React.Component {
   render() {
     return (
-      <div className="information">
+      <div id="info-top" className="information">
           <Link className="back-link" to="/">home</Link> 
           <h2>jump to section</h2>
           <div id="jump-section">
@@ -19,7 +19,7 @@ export default class Information extends React.Component {
               <section id={elem.short_title}>
                 {elem.content}
                 <br/>
-                <a href="#jump-section">back to top</a>
+                <a href="#info-top">back to top</a>
               </section>
             )})}
       </div>
