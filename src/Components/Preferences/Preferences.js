8a5c282158c02eaa800a14e161cf8aa736447540
Michael Dera <michael@devshop.co.zw>
2019-01-05 20:26:32 +0200

    toggle button up

--- src/Components/Preferences/Preferences.js
+++ src/Components/Preferences/Preferences.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './preferences.css'
 
-
- export default class Preferences extends Component {
+ export default class Preferences extends React.Component {
     render() {
         const bottom = (this.props.status === "open")? "0": "-290px";
         return (
             <div className="preferences" style={{ bottom : bottom }} >                
+                <button className="close-btn" onClick={this.props.toggle()}>close</button>           
                 <label htmlFor="font-size-input" className="font-size-label">font-size</label>
                 <input name="font-size-input" className="font-size-input" type="range" min="8" max="30" placeholder={11} onChange={this.props.handleFontSizeChange()}/>
                 <div className="preference-control">
                     <button className="btn-mode" onClick={this.props.toggleMode()}>{(this.props.mode === "day" )? "night mode" :  "day mode"}</button>
                 </div>
-                <button className="close-btn" onClick={this.props.toggle()}>close</button>
             </div>
         );
     }
