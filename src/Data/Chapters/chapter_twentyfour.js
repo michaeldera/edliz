b4de1e9a2286aef0dedc6ac3e0fd2aec275a7b87
Michael Dera <michael@devshop.co.zw>
2019-01-06 14:35:41 +0200

    refactoring

---
+++ src/Data/Chapters/chapter_twentyfour.js
@@ -0,0 +1,16 @@
+export default chapter_twentyfour = {
+    short_title: "Chapter 24",
+    long_title: "Skin Conditions",
+    subsections: [
+      {
+        short_title: "Section 0",
+        long_title: "Notes",
+        content:
+          <span>
+          </span>
+      },
+    ],
+    content:
+      <span>
+      </span>
+  }
