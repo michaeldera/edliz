08457952bc507733dfd2243f6658db87ffd6bb94
Michael Dera <michael@devshop.co.zw>
2019-01-15 11:27:18 +0200

    added overlay component

---
+++ src/Views/Overlay/Overlay.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './overlay.css'
+
+export class Overlay extends Component {
+    render() {
+        return (
+            <div className="overlay">
+             </div>   
+        );
+    }
+}
+
+export default Header;
