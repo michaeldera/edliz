47d41ad18307b248393144544abfab86138b3576
Michael Dera <michael@devshop.co.zw>
2019-01-18 08:53:58 +0200

    typo

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,7 +10,7 @@ export default class Home extends React.Component {
             Drugs and<br/> 
             Medicines List <br/>
             in Zimbabwe </h1>
-          <Link className="btn-read-guidelines" to="/chapters/1">Read Guideines...</Link> 
+          <Link className="btn-read-guidelines" to="/chapters/1">Read Guidelines...</Link> 
           <p className="edliz-intro">This 7th essential medicines list and standard treatment guidelines for the most common health conditions in Zimbabwe has been endorsed by the National Medicine & Therapeutics Policy Advisory Committee. It is the product of many years of combined efforts by hundreds of health workers at all levels of the health care system in Zimbabwe</p>
           <Link className="btn-settings" to="/settings">Settings</Link> <Link className="btn-settings" to="/information">Information</Link> 
       </div>
