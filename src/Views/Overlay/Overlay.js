825187d60cc3cd9d2c60d25e7fbe584530a349a0
Michael Dera <michael@devshop.co.zw>
2019-01-22 22:17:59 +0200

    inline style on overlay

--- src/Views/Overlay/Overlay.js
+++ src/Views/Overlay/Overlay.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import './overlay.css'
 
 export default class Overlay extends Component {
-    render() {
-        return (
-            <div className="overlay" style={{ visibility : this.props.visibility}} onClick={this.props.dismiss}>
-             </div>   
-        );
+    render() {    
+        let overlayStyle = {
+            background:"rgba(0, 0, 0, 0.1)",
+            height: "100%",
+            left:"0",
+            position: "fixed",
+            top:"0",
+            visibility: this.props.visibility,
+            width: "100%",
+            zIndex: "998"
+        }
+        return <div style={overlayStyle} onClick={this.props.dismiss}></div>;
     }
 }
 
