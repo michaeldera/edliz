1e4fde92941149c1258ed5cdf0c0e2a5e535350e
Michael Dera <michael@devshop.co.zw>
2019-01-22 21:47:23 +0200

    removed panel button

--- src/Components/Preferences/Preferences.js
+++ src/Components/Preferences/Preferences.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import close from './chevron-down.svg';
-import './preferences.css'
+import './preferences.css';
 import Overlay  from '../../Views/Overlay/Overlay';
 
 export default class Preferences extends React.Component {
@@ -10,7 +9,6 @@ export default class Preferences extends React.Component {
         return (
             <>
                 <div className="preferences" style={{ bottom: bottom }} >
-                    <button className="close-btn" onClick={this.props.toggle()}><img src={close} alt="close" /></button>
                     <label htmlFor="font-size-input" className="font-size-label">font-size</label>
                     <input name="font-size-input" className="font-size-input" type="range" min="8" max="30" placeholder={11} onChange={this.props.handleFontSizeChange()} />
                     <div className="preference-control">
