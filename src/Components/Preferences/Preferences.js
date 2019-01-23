3dfafb4b4a53116c29623f170794c00dfbc60b4a
Michael Dera <michael@devshop.co.zw>
2019-01-23 10:00:57 +0200

    added DrawerButton

--- src/Components/Preferences/Preferences.js
+++ src/Components/Preferences/Preferences.js
@@ -3,12 +3,13 @@ import './preferences.css';
 import Overlay  from '../../Views/Overlay/Overlay';
 
 export default class Preferences extends React.Component {
-    render() {
+    render(){
         const bottom = (this.props.status === "open") ? "0" : "-290px";
         const  overlayVisibility = (this.props.status === "open")?  "visible" : "hidden";
         return (
             <>
-                <div className="preferences" style={{ bottom: bottom }} >
+                <div className="preferences" style={{ bottom: bottom }}>
+                    <DrawerButton/>
                     <label htmlFor="font-size-input" className="font-size-label">font-size</label>
                     <input name="font-size-input" className="font-size-input" type="range" min="8" max="30" placeholder={11} onChange={this.props.handleFontSizeChange()} />
                     <div className="preference-control">
@@ -18,5 +19,19 @@ export default class Preferences extends React.Component {
                 <Overlay visibility={overlayVisibility} dismiss={this.props.toggle()}/>
             </>
         );
+    }
+}
+
+class DrawerButton extends React.Component {
+    render(){
+        let drawerButtonStyle = {
+            backgroundColor: "rgba( 255, 255, 255, 0.86)",
+            borderRadius: "4px",
+            display: "block", 
+            height:"6px",
+            margin: "0.68rem auto 2.8rem auto",
+            width: "2.6rem"
+        };
+        return <div style={drawerButtonStyle}></div>;
     }
 }
