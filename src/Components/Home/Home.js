8a74d81d128ae27141c0b83cf05a1500aa10e199
Michael Dera <michael@devshop.co.zw>
2019-01-27 15:43:39 +0200

    Color Scheme change

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -12,7 +12,7 @@ export default class Home extends React.Component {
             in Zimbabwe </h1>
           <Link className="btn-read-guidelines" to="/chapters/1">Read Guidelines...</Link> 
           <p className="edliz-intro">This 7th essential medicines list and standard treatment guidelines for the most common health conditions in Zimbabwe has been endorsed by the National Medicine & Therapeutics Policy Advisory Committee. It is the product of many years of combined efforts by hundreds of health workers at all levels of the health care system in Zimbabwe</p>
-          <Link className="btn-settings" to="/settings">Settings</Link> <Link className="btn-settings" to="/information">Information</Link> 
+          <Link className="btn-settings" to="/settings">SETTINGS</Link> <Link className="btn-settings" to="/information">INFORMATION</Link> 
       </div>
     );
   }
--- src/Views/Overlay/Overlay.js
+++ src/Views/Overlay/Overlay.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class Overlay extends Component {
     render() {    
         let overlayStyle = {
-            background:"rgba(0, 0, 0, 0.1)",
+            backgroundColor: "#21D4FD",
+            backgroundImage: "linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)", 
+            opacity: "0.1",
             height: "100%",
             left:"0",
             position: "fixed",
