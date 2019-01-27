ab2b76347256e8a2a70d5ecdfecfe73c18fd8660
mcheartno <noel@namesandfaces.co>
2019-01-27 11:15:49 +0200

    wrap the App in the Provider to access the stores

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'mobx-react'
+import EdlizStore from './stores/EdlizStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = (
+    <Provider EdlizStore={EdlizStore}>
+        <App/>
+    </Provider>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
