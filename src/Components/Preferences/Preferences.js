b4d3290cc75bb4007bc44f179ebbb0d0d921f329
Noel Moyo <noel@namesandfaces.com>
2019-02-07 00:38:30 +0200

    refactored the components as well as added functions to handle the mode, preference modal and font size change

--- src/Components/Preferences/Preferences.js
+++ src/Components/Preferences/Preferences.js
@@ -4,21 +4,54 @@ import './preferences.css'
 import Overlay  from '../../Views/Overlay/Overlay';
 
 export default class Preferences extends React.Component {
-    render() {
-        const bottom = (this.props.status === "open") ? "0" : "-290px";
-        const  overlayVisibility = (this.props.status === "open")?  "visible" : "hidden";
-        return (
-            <>
-                <div className="preferences" style={{ bottom: bottom }} >
-                    <button className="close-btn" onClick={this.props.toggle()}><img src={close} alt="close" /></button>
-                    <label htmlFor="font-size-input" className="font-size-label">font-size</label>
-                    <input name="font-size-input" className="font-size-input" type="range" min="8" max="30" placeholder={11} onChange={this.props.handleFontSizeChange()} />
-                    <div className="preference-control">
-                        <button className="btn-mode" onClick={this.props.toggleMode()}>{(this.props.mode === "day") ? "night mode" : "day mode"}</button>
-                    </div>
-                </div>
-                <Overlay visibility={overlayVisibility} dismiss={this.props.toggle()}/>
-            </>
-        );
-    }
+  handlePreferences = () => {
+    const {preferences, toggle} = this.props
+    toggle(preferences)
+  }
+
+  handleMode = () => {
+    const {mode, toggleMode} = this.props
+    toggleMode(mode)
+  }
+
+  handleFontSizeChange = event => {
+    this.props.fontSizeChange(event.target.value)
+  }
+
+  render() {
+    const {preferences, mode} = this.props
+    const bottom = (preferences === "open") ? "0" : "-290px";
+    const  overlayVisibility = (preferences === "open")?  "visible" : "hidden";
+    return (
+      <>
+        <div className="preferences" style={{ bottom: bottom }} >
+          <button
+            className="close-btn"
+            onClick={this.handlePreferences}><img src={close}alt="close" />
+          </button>
+          <label
+            htmlFor="font-size-input"
+            className="font-size-label"
+          >
+            font-size
+          </label>
+          <input
+            name="font-size-input"
+            className="font-size-input"
+            type="range" min="8"
+            max="30" placeholder={11}
+            onChange={this.handleFontSizeChange}
+          />
+          <div className="preference-control">
+            <button className="btn-mode"
+                    onClick={this.handleMode}
+            >
+              {(mode === "day") ? "night mode" : "day mode"}
+            </button>
+          </div>
+        </div>
+        <Overlay visibility={overlayVisibility} dismiss={this.handlePreferences}/>
+      </>
+    );
+  }
 }
