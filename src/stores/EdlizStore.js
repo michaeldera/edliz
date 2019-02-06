c71c53f4892ba5bb23bfd2195fbc0f5a74391e25
Noel Moyo <noel@namesandfaces.com>
2019-02-07 00:35:11 +0200

    created the store for the project, added observables and actions that are utilised by the components

--- src/stores/EdlizStore.js
+++ src/stores/EdlizStore.js
@@ -4,10 +4,33 @@ import { book } from '../Data/data'
 
 class EdlizStore {
   fontSize = 11
-  navigation = 'open'
+  navigation = 'closed'
   mode = 'day';
-  preference = 'closed'
+  preferences = 'closed'
   books = book
+
+  togglePreferences = (preferences) => {
+
+    if (this.navigation === 'open') {
+      this.toggleNavigationPanel()
+    }
+    this.preferences = preferences === 'open' ? 'closed' : 'open'
+  }
+
+  toggleNavigationPanel = navigation =>{
+    if (this.preferences === "open") {
+      this.togglePreferences();
+    }
+    this.navigation = navigation === 'open' ? 'closed' : 'open'
+  }
+
+  toggleMode = mode => {
+    this.mode = mode === "night" ? "day" : "night"
+  }
+
+  fontSizeChange = fontSize =>{
+    this.fontSize = fontSize
+  }
 
 }
 
@@ -15,8 +38,12 @@ decorate(EdlizStore, {
   fontSize: observable,
   navigation: observable,
   mode: observable,
-  preference: observable,
-  books: observable
+  preferences: observable,
+  books: observable,
+  togglePreferences: action,
+  toggleNavigationPanel: action,
+  toggleMode: action,
+  fontSizeChange: action
 })
 
 const store = new EdlizStore()
