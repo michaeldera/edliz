ce2932076894ebcb8616e3dd49851cd31a64152a
Michael Dera <michael@devshop.co.zw>
2019-02-11 10:03:51 +0200

    fixed issue with routing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 import Reader from './Components/Reader/Reader';
 import Home from './Components/Home/Home';
@@ -11,14 +11,14 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
+        <HashRouter>
           <Switch>
             <Route exact path="/" component={Home}/>
             <Route path="/chapters/:chapterNumber" children={RenderReader} />
             <Route path="/information" component={Information}/>
             <Route path="/settings" component={Settings}/>
           </Switch>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     );
   }
