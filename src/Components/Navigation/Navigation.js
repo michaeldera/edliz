b72a71ab2d22a4e44f7ffd4a99e37d189ed84e82
Noeza <mcheart90@gmail.com>
2019-03-21 23:22:40 +0200

    changed the navigation bar content to be for the bigger screen size design.

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,39 +1,40 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
 import './navigation.css'
 import NavigationItem from '../../Views/NavigationItem/NavigationItem';
-import Overlay  from '../../Views/Overlay/Overlay';
+import ChapterImg from './chapter.png'
 
 class Navigation extends Component {
-
-  handleNavigation = () => {
-    const {navigation, toggle} = this.props
-    toggle(navigation)
-  }
-
   render() {
-    const {navigation, content} = this.props
-    const  overlayVisibility = (navigation === "open")?  "visible" : "hidden";
+    const {chapter, content, title} = this.props
     return (
-      <>
-        <nav className={navigation} >
-          <div className="nav-header">
-            <h2 className="nav-heading">Essential Drugs and Medicines List in Zimbabwe</h2>
-            <Link className="nav-header-btn" to="/">Home</Link>
-            <Link className="nav-header-btn" to="/information">Information</Link>
-            <Link className="nav-header-btn" to="/settings">Settings</Link>
+      <div className="screen">
+        <div className="left-navigation">
+          <img className="chapter-img" src={ChapterImg} alt="" width="179" height="254" />
+          <div className="chapter-list">
+          { content.chapters.map( (section ,  index ) => (
+                <NavigationItem key={index} section={section} chapter={index + 1} />
+              ))}
           </div>
-          <div className="tab-content">
-            { content.chapters.map( (section ,  index ) => (
-              <NavigationItem clicked={this.handleNavigation} key={index} section={section} chapter={index + 1} />
-            ))}
-          </div>
-        </nav>
-        <Overlay visibility={overlayVisibility} dismiss={this.handleNavigation}/>
-      </>
+        </div>
+        <div className="chapter-content">
+            <div className="chapter-title">
+               {title}
+            </div>
+            <div className="chapter-title">{chapter.long_title}</div>
+            <div className="article">
+                    {(chapter.subsections)? chapter.subsections.map( section =>  {
+                        return (
+                          <>
+                          <section key={section.short_title}>{section.content}</section>
+                          </>
+                        )
+                    }) : chapter.content }
+            </div>
+        </div>
+      </div>
     );
   }
 }
 
 
-export default Navigation;
+export default Navigation
