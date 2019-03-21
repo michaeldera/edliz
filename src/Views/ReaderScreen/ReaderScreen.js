97401e2705c15e37a87211fefe71fb035e366660
Noeza <mcheart90@gmail.com>
2019-03-21 23:29:12 +0200

    added navigation to component

--- src/Views/ReaderScreen/ReaderScreen.js
+++ src/Views/ReaderScreen/ReaderScreen.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import Navigation from '../../Components/Navigation/Navigation'
 import './readerscreen.css'
 
 export default class ReaderScreen extends Component {
     render() {
+        const {content, navigation, toggle, title, chapter} = this.props
         return (
-            <div className="screen">
-                <div className="article" style={this.props.style}>
-                    {(this.props.content.subsections)? this.props.content.subsections.map( section =>  {
-                        return <section key={section.short_title}>{section.content}</section>
-                    }) : this.props.content.content }
-                </div>
+            <div>
+               <Navigation
+               content={content}
+               chapter={chapter}
+               navigation={navigation}
+               toggle={toggle}
+               title={title}
+               />
             </div>
         );
     }
