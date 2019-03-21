cd434a1654f727d1aa9a88829a61cff1d8ca106b
Noeza <mcheart90@gmail.com>
2019-03-21 23:21:00 +0200

    added navigation menu

---
+++ src/Components/MainMenu/MainMenu.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './MainMenu.css'
+import menu from './../../Assets/Icons/navigation.svg';
+
+export default class MainMenu extends React.Component{
+
+  handleNavigation = () => {
+    const {navigation, toggle} = this.props
+    toggle(navigation)
+  }
+
+  render () {
+    const {reader} = this.props
+    return (
+      <>
+        {reader && (
+            <button className="btn-toggle-nav" onClick={this.handleNavigation}>
+              <img src={menu} alt="menu" />
+            </button>
+        )}
+        <ul className='menu-list'>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/chapters/1">Start Reading</Link></li>
+          <li><Link  to="/information">About this Project</Link></li>
+        </ul>
+        </>
+    )
+  }
+}
