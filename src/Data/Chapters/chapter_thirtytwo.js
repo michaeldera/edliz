be9b6c343a3a4b51b8b7f706440b342caeec3612
Noeza <mcheart90@gmail.com>
2019-03-30 11:58:33 +0200

    refactored the files to remove the a tag and apply linting rules

--- src/Data/Chapters/chapter_thirtytwo.js
+++ src/Data/Chapters/chapter_thirtytwo.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-export let chapter_thirtyone = {
-    short_title: "Chapter 32",
-    long_title: "Chapter 32",
-    content: <span></span>
+export const chapter_thirtytwo = {
+    short_title: 'Chapter 32',
+    long_title: 'Chapter 32',
+    content: <span />,
 }
--- src/Data/Chapters/chapter_twentyfour.js
+++ src/Data/Chapters/chapter_twentyfour.js
@@ -1,18 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-export let chapter_twentyfour = {
-    short_title: "Chapter 24",
-    long_title: "Skin Conditions",
+export const chapter_twentyfour = {
+    short_title: 'Chapter 24',
+    long_title: 'Skin Conditions',
     subsections: [
-      {
-        short_title: "Section 0",
-        long_title: "Notes",
-        content:
-          <span>
-          </span>
-      },
+        {
+            short_title: 'Section 0',
+            long_title: 'Notes',
+            content: <span />,
+        },
     ],
-    content:
-      <span>
-      </span>
-  }
+    content: <span />,
+}
