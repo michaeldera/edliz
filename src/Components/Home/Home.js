771663c36a97231be967b7728ed1af6cdd14fd75
Michael <michaeldera@live.com>
2019-05-10 22:27:03 +0200

    Revert "Feature/home page"

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,47 +1,27 @@
 import React from 'react';
-import { Link } from "react-router-dom"
-import Media from 'react-media'
-import MainMenu from '../MainMenu/MainMenu'
-import RoundedBtn from '../RoundedBtn/RoundedBtn'
-import WheelChair from './home-wheelchair.png'
-import Github from './github.svg'
+import { Link } from "react-router-dom";
 import './home.css';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div className="main">
-        <MainMenu/>
-        <div className="home">
-          <div className="left-row">
-            <div className="main-title"> Essential Drugs & <br /> Medicines List <br /> in Zimbabwe</div>
-              <div className="edliz-intro">
-                    This 7th essential medicines list and standard treatment guidelines for the most common
-                    health conditions in Zimbabwe has been endorsed by the National Medicine & Therapeutics
-                    Policy Advisory Committee. It is the product of many years of combined efforts by hundreds
-                    of health workers at all levels of the health care system in Zimbabwe.
-              </div>
-              <p className="start-reading">
-                 <RoundedBtn href="/#/chapters/1" type="a">
-                    Start Reading
-                 </RoundedBtn>
-              </p>
-          </div>
-          <Media
-              query="(min-width: 1280px)"
-              render={() => <img className="people-wide" src={WheelChair} alt="" width="809" height="650"/>}
-          />
-        </div>
-        <div className="github">
-          <a href="https://github.com/michaeldera/edliz" className="github-link">
-              {' '}
-              <img className="people-wide" src={Github} alt="" width="33" height="32" />
-          </a>
-            <p>
-              This application was built and is maintained by a well meaning developers. The code is
-              opensource and is available to fork, share and improve on github.
-            </p>    
-         </div>
+      <div className="home">
+        <h1 className="main-title">Essential<br/>
+          Drugs and<br/>
+          Medicines List <br/>
+          in Zimbabwe
+        </h1>
+        <Link className="btn-read-guidelines" to="/chapters/1">Read Guidelines...</Link>
+        <p className="edliz-intro">
+          This 7th essential medicines list and standard treatment guidelines for the most
+          common health conditions in Zimbabwe has been endorsed by the National Medicine &
+          Therapeutics Policy Advisory Committee. It is the product of many years of combined
+          efforts by hundreds of health workers at all levels of the health care system in Zimbabwe
+        </p>
+        <Link className="btn-settings" to="/settings">
+          Settings
+        </Link>
+        <Link className="btn-settings" to="/information">Information</Link>
       </div>
     );
   }
