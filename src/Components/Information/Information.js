7e24b86918c84aa046f02389ff0e02102577d11b
Noeza <mcheart90@gmail.com>
2019-05-12 22:41:26 +0200

    refactored the Information Page and added scroll to top buttons

--- src/Components/Information/Information.js
+++ src/Components/Information/Information.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import './information.css';
-import { book } from '../../Data/data'
+import React from 'react'
+import {inject, observer} from 'mobx-react/index'
+import {toJS} from 'mobx/lib/mobx'
+import {Link, animateScroll as scroll} from 'react-scroll'
+import './information.css'
+import {book} from '../../Data/data'
+import MainMenu from '../MainMenu/MainMenu'
 
-export default class Information extends React.PureComponent {
-  render() {
-    return (
-      <div id="info-top" className="information">
-        <Link className="back-link" to="/">home</Link>
-        <h2>jump to section</h2>
-        <div id="jump-section">
-          {book.contents.pre.map((elem, index, array) =>
-            <a href={"#" + elem.short_title} key={index}>{elem.short_title}</a>)}
-        </div>
-        {book.contents.pre.map((elem, index, array) =>
-            <section id={elem.short_title} key={index}>
-              {elem.content}
-              <br/>
-              <a href="#info-top">back to top</a>
-            </section>
-          )}
-      </div>
-    );
-  }
+class Information extends React.PureComponent {
+    scrollToTop = () => {
+        scroll.scrollToTop()
+    }
+
+    render() {
+        const {toggleNavigationPanel, navigation} = toJS(this.props.EdlizStore)
+
+        return (
+            <div>
+                <MainMenu toggle={toggleNavigationPanel} navigation={navigation} />
+                <div className="information">
+                    <div className="jump">Jump to Section</div>
+                    <div>
+                        <ul>
+                            {book.contents.pre.map((elem) => (
+                                <li key={elem.short_title}>
+                                    {/* <a href='javascript:;' onClick={() => this.scrollTo(elem.short_title)}>{elem.short_title}</a> */}
+                                    <Link
+                                        activeClass="active"
+                                        className="link"
+                                        to={elem.short_title}
+                                        spy
+                                        smooth
+                                        offset={-70}
+                                        duration={500}
+                                    >
+                                        {elem.short_title}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    {book.contents.pre.map((elem) => (
+                        <section className={elem.short_title} key={elem.short_title}>
+                            {elem.content}
+                            <br />
+                            <button type="button" className="depthButton" onClick={this.scrollToTop}>
+                                Back to top
+                            </button>
+                        </section>
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
+
+export default inject('EdlizStore')(observer(Information))
