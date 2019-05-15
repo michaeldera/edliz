747cbba87cbb667fcbb393fc0d1631a4279a1e05
Michael Dera <michaeldera@live.com>
2019-05-15 19:23:18 +0200

    ui improvements

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 import Media from 'react-media'
 import MainMenu from '../MainMenu/MainMenu'
 import RoundedBtn from '../RoundedBtn/RoundedBtn'
-import WheelChair from './home-wheelchair.png'
 import Github from './github.svg'
+import medicine from './medicine.svg'
 import './home.css';
 
 export default class Home extends React.Component {
@@ -15,7 +15,7 @@ export default class Home extends React.Component {
         <div className="home">
           <div className="left-row">
             <div className="main-title"> Essential Drugs &amp; Medicines <br/> List in Zimbabwe</div>
-              <div className="edliz-intro">
+              <div className="introduction">
                     This 7th essential medicines list and standard treatment guidelines for the most common
                     health conditions in Zimbabwe has been endorsed by the National Medicine &amp; Therapeutics
                     Policy Advisory Committee. It is the product of many years of combined efforts by hundreds
@@ -29,18 +29,17 @@ export default class Home extends React.Component {
           </div>
           <Media
               query="(min-width: 1280px)"
-              render={() => <img className="people-wide" src={WheelChair} alt="" width="809" height="650"/>}
+              render={() => <img className="medicine-illustration" src={medicine} alt=""/>}
           />
         </div>
         <div className="github">
           <a href="https://github.com/michaeldera/edliz" className="github-link">
               {' '}
-              <img className="people-wide" src={Github} alt="" width="33" height="32" />
+              <img className="people-wide" src={Github} alt="" width="20" height="20" />
           </a>
-            <p>
-              This application was built and is maintained by a well meaning developers. The code is
-              opensource and is available to fork, share and improve on github.
-            </p>    
+          <small>
+              This application was built and is maintained by a well meaning developers. The code is opensource and is available to fork, share and improve on github.
+            </small>    
          </div>
       </div>
     );
