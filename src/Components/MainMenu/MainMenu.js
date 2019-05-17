8d2bdbaf0bcae5df140eddf33f0f8224fc7442c7
Michael Dera <michaeldera@live.com>
2019-05-17 13:48:13 +0200

    refactored code to add Hamburger button.

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -1,29 +1,19 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './MainMenu.css'
-import bars from '../../Assets/Icons/menu.svg'
+import HamburgerButton from '../../Views/HamburgerButton/HamburgerButton';
 
 export default class MainMenu extends React.Component {
     render () {
         return (
-            <header>    
-                <HamburgerButton/>             
+            <header>  
+                <HamburgerButton/>
                  <nav>
                     <Link to="/">Home</Link>
                     <Link to="/chapters/1">Start Reading</Link>
                     <Link to="/information">About this Project</Link>
                  </nav>
             </header>
-        )
-    }
-}
-
-class HamburgerButton extends React.Component {
-    render(){
-        return (
-            <button style={{position:"absolute", top: 0, left: 0}} className="btn-toggle-nav">
-                    <img src={bars} alt="bars"/>
-            </button>
         )
     }
 }
