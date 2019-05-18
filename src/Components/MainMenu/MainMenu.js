35200e63b341c09475060459ee7265caa3bf2471
Michael Dera <michaeldera@live.com>
2019-05-18 10:15:49 +0200

    better navigation experience

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -4,10 +4,22 @@ import './MainMenu.css'
 import HamburgerButton from '../../Views/HamburgerButton/HamburgerButton';
 
 export default class MainMenu extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { visible: false };
+        this.toggleNavigation = this.toggleNavigation.bind(this);
+    }
+
+    toggleNavigation = () => {
+        this.setState((state) => ({
+            visible: !state.visible
+        }));
+    }
+
     render () {
         return (
-            <header>  
-                <HamburgerButton/>
+            <header className={this.state.visible? "nav-on": "nav-off"}>  
+                <HamburgerButton action={this.toggleNavigation}/>
                  <div className="main-nav">
                     <Link to="/">Home</Link>
                     <Link to="/chapters/1">Start Reading</Link>
--- src/Views/HamburgerButton/HamburgerButton.js
+++ src/Views/HamburgerButton/HamburgerButton.js
@@ -4,7 +4,7 @@ import bars from './menu.svg'
 export default class HamburgerButton extends React.Component {
     render(){            
         return (
-            <button style={{position:"fixed", top: 0, left:0, border: "none", borderRadius:"50%", padding: 15, background: "transparent", width: 50, height: 50, top: 0, left: 0}} onClick={this.props.action} className="btn-toggle-nav">
+            <button style={{position:"fixed", top: 0, left:0, zIndex: 99999, border: "none", padding: 16, margin:0, boxSizing: "border-box", background: "#fff", width: 50, height: 50, top: 0, left: 0}} onClick={this.props.action} className="btn-toggle-nav">
                 <img src={bars} alt="bars"/>
             </button>
         )
