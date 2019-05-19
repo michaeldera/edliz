571c901080b08fef98c1bd7a6c44e097cfbc5e84
Michael Dera <michaeldera@live.com>
2019-05-19 14:35:45 +0200

    moved css to separate files

--- src/Views/HamburgerButton/HamburgerButton.js
+++ src/Views/HamburgerButton/HamburgerButton.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import bars from './menu.svg'
+import './hamburgerbutton.css'
 
 export default class HamburgerButton extends React.Component {
     render(){            
         return (
-            <button style={{position:"fixed", top: 0, left:0, zIndex: 99999, border: "none", padding: 16, margin:0, boxSizing: "border-box", background: "#fff", width: 50, height: 50, top: 0, left: 0}} onClick={this.props.action} className="btn-toggle-nav">
+            <button className="hamburger-button" onClick={this.props.action} className="btn-toggle-nav">
                 <img src={bars} alt="bars"/>
             </button>
         )
