808b21e30e41246513bc2900f4222110bec9b52b
Michael Dera <michaeldera@live.com>
2019-05-19 14:43:57 +0200

    multiple classname attr fix

--- src/Views/HamburgerButton/HamburgerButton.js
+++ src/Views/HamburgerButton/HamburgerButton.js
@@ -5,7 +5,7 @@ import './hamburgerbutton.css'
 export default class HamburgerButton extends React.Component {
     render(){            
         return (
-            <button className="hamburger-button" onClick={this.props.action} className="btn-toggle-nav">
+            <button onClick={this.props.action} className="hamburger-button btn-toggle-nav">
                 <img src={bars} alt="bars"/>
             </button>
         )
