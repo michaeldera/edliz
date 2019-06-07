12e486e69924d576ef272515ba7f6be6907929c3
Michael Dera <michaeldera@live.com>
2019-06-07 19:10:46 +0200

    changed bgcolor

--- src/Views/HamburgerButton/HamburgerButton.js
+++ src/Views/HamburgerButton/HamburgerButton.js
@@ -5,7 +5,7 @@ import './hamburgerbutton.css'
 export default class HamburgerButton extends React.Component {
     render(){            
         return (
-            <button onClick={this.props.action} className="hamburger-button btn-toggle-nav">
+            <button onClick={this.props.action} className="hamburger-button">
                 <img src={bars} alt="bars"/>
             </button>
         )
