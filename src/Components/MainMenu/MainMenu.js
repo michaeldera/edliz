0595c2805d9000cfc4004ad2ce2eef05665f2103
Michael Dera <michaeldera@live.com>
2019-06-07 21:37:16 +0200

    changed icons

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Media from 'react-media'
 import './MainMenu.css'
+import home from './home.svg'
+import read from './read.svg'
 import info from './information.svg'
 import HamburgerButton from '../../Views/HamburgerButton/HamburgerButton';
 
@@ -23,8 +25,8 @@ export default class MainMenu extends React.Component {
             <header className={this.state.visible? "nav-on": "nav-off"}>  
             <Media query="(max-width: 780px)" render={() => <HamburgerButton action={this.toggleNavigation}/>} />
                  <div className="main-nav">
-                    <Link to="/"><img src={info} alt="information icon"/>Home</Link>
-                    <Link to="/chapters/1"><img src={info} alt="information icon" />Start Reading</Link>
+                    <Link to="/"><img src={home} alt="information icon"/>Home</Link>
+                    <Link to="/chapters/1"><img src={read} alt="information icon" />Start Reading</Link>
                     <Link to="/information"><img src={info} alt="information icon" />About this Project</Link>
                  </div>
             </header>
