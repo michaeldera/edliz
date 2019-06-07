44748452c796b1a6aed3c246c13fcda6eda63548
Michael Dera <michaeldera@live.com>
2019-06-07 21:04:23 +0200

    main-menu icon styling

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Media from 'react-media'
 import './MainMenu.css'
+import info from './information.svg'
 import HamburgerButton from '../../Views/HamburgerButton/HamburgerButton';
 
 export default class MainMenu extends React.Component {
@@ -22,9 +23,9 @@ export default class MainMenu extends React.Component {
             <header className={this.state.visible? "nav-on": "nav-off"}>  
             <Media query="(max-width: 780px)" render={() => <HamburgerButton action={this.toggleNavigation}/>} />
                  <div className="main-nav">
-                    <Link to="/">Home</Link>
-                    <Link to="/chapters/1">Start Reading</Link>
-                    <Link to="/information">About this Project</Link>
+                    <Link to="/"><img src={info} alt="information icon"/>Home</Link>
+                    <Link to="/chapters/1"><img src={info} alt="information icon" />Start Reading</Link>
+                    <Link to="/information"><img src={info} alt="information icon" />About this Project</Link>
                  </div>
             </header>
         )
