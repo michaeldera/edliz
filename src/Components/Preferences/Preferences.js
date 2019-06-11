e66c4978ad3a34b08a6a825ce2bf3dab15203ab5
Michael Dera <michaeldera@live.com>
2019-06-11 21:26:18 +0200

    mode button :)

--- src/Components/Preferences/Preferences.js
+++ src/Components/Preferences/Preferences.js
@@ -91,21 +91,25 @@ class DrawerButton extends React.PureComponent {
 
 class ModeButton extends React.PureComponent {
   render(){
-      let modeButtonStyle = {
+      const modeButtonStyle = {
           borderRadius: "4px",
           backgroundColor:"#6c63ff",
           border: "none",
-          color:"#ccf",
-          fontFamily: "'Muli' sans-serif",
-          margin:"26px",
-          width:"5rem",
-          padding: "8px 16px",
+          width:"36px",
+          height:"36px",
+          margin: "26px",
+          borderRadius: "50%",
+          padding: "8px",
           outline: "none"
       };
 
+      const {mode} = this.props
+
       return (
            <button style={modeButtonStyle} onClick={this.props.toggle}>
-              {(this.props.mode === "day") ? "NIGHT" : "DAY"}
+              <svg height="20px" xmlns="http://www.w3.org/2000/svg" fill={ mode === "day"? "#fff": "#2a2a2a"} viewBox="0 0 20 20">
+                <path d="M10 14a4 4 0 1 1 0-8 4 4 0 0 1 0 8zM9 1a1 1 0 1 1 2 0v2a1 1 0 1 1-2 0V1zm6.65 1.94a1 1 0 1 1 1.41 1.41l-1.4 1.4a1 1 0 1 1-1.41-1.41l1.4-1.4zM18.99 9a1 1 0 1 1 0 2h-1.98a1 1 0 1 1 0-2h1.98zm-1.93 6.65a1 1 0 1 1-1.41 1.41l-1.4-1.4a1 1 0 1 1 1.41-1.41l1.4 1.4zM11 18.99a1 1 0 1 1-2 0v-1.98a1 1 0 1 1 2 0v1.98zm-6.65-1.93a1 1 0 1 1-1.41-1.41l1.4-1.4a1 1 0 1 1 1.41 1.41l-1.4 1.4zM1.01 11a1 1 0 1 1 0-2h1.98a1 1 0 1 1 0 2H1.01zm1.93-6.65a1 1 0 1 1 1.41-1.41l1.4 1.4a1 1 0 1 1-1.41 1.41l-1.4-1.4z" />
+              </svg>
            </button>
       )
   }
