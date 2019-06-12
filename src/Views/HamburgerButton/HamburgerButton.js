2a48d4270927751d5031abf43730fdee0fabec03
Michael Dera <michaeldera@live.com>
2019-06-12 16:02:00 +0200

    modify radius on click

--- src/Views/HamburgerButton/HamburgerButton.js
+++ src/Views/HamburgerButton/HamburgerButton.js
@@ -5,7 +5,7 @@ export default class HamburgerButton extends React.Component {
     render(){  
         const {action, IsActive } = this.props;          
         return (
-            <button onClick={action} className="hamburger-button">
+            <button onClick={action} style={{borderRadius: IsActive? 0: "50%" }} className="hamburger-button">
                 <svg fill="#6c63ff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     { IsActive ? 
                         <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" /> :
