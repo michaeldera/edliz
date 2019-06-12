203a5e592bc830a4100f115255e90dea60cb648f
Michael Dera <michaeldera@live.com>
2019-06-12 14:16:22 +0200

    nav svg icon

--- src/Views/HamburgerButton/HamburgerButton.js
+++ src/Views/HamburgerButton/HamburgerButton.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import bars from './menu.svg'
 import './hamburgerbutton.css'
 
 export default class HamburgerButton extends React.Component {
     render(){            
         return (
             <button onClick={this.props.action} className="hamburger-button">
-                <img src={bars} alt="bars"/>
+                <svg fill="#6c63ff"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+                </svg>
             </button>
         )
     }
