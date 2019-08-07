96b1ae0a5f01e7e9a33cc00d57fc217156d95903
Noeza <mcheart90@gmail.com>
2019-08-07 23:34:23 +0200

    added typings to Book chapters

--- src/Data/Chapters/chapter_thirtyone.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export let chapter_thirtyone = {
-    short_title: "Chapter 31",
-    long_title: "Chapter 31",
-    content: <span></span>
-}
--- src/Data/Chapters/chapter_thirtytwo.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export let chapter_thirtytwo = {
-    short_title: "Chapter 32",
-    long_title: "Chapter 32",
-    content: <span></span>
-}
