61beea9b0f319737339ef6af8757a461ab2edeeb
Michael Dera <michaeldera@live.com>
2020-06-10 10:37:49 +0200

    added application insights.

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace EDLIZ
             {
                 configuration.RootPath = "Client/build";
             });
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
