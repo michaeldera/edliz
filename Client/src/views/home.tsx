5c5b369c44cd65d4dae94cde4c472b8e6e83217b
Michael Dera <michaeldera@live.com>
2020-06-13 10:17:45 +0200

    switched to browser router

--- Client/src/views/home.tsx
+++ Client/src/views/home.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { PrimaryButton, Text, Stack, IStackTokens, Image } from '@fluentui/react';
 import { logo } from '../assets';
+import { useHistory } from 'react-router-dom';
 
 export const Home = () => {
+    let history = useHistory();
 
     const stackTokens: IStackTokens = {
         childrenGap: 40,
@@ -14,13 +16,17 @@ export const Home = () => {
         paddingBottom: 20
     }
 
+    const handleStartReading = () => {
+        history.push('/chapters/1');
+    }
+
     return (
         <Stack tokens={stackTokens}>
             <Text variant="xxLarge" block>Essential Drugs and Medicines List in Zimbabwe</Text>
             <Image src={logo} alt="Artwork" style={imageStyle}/>
             <Text block>This 7th essential medicines list and standard treatment guidelines for the most common health conditions in Zimbabwe has been endorsed by the National Medicine & Therapeutics Policy Advisory Committee. It is the product of many years of combined efforts by hundreds of health workers at all levels of the health care system in Zimbabwe.</Text>
             <Stack horizontal>
-                <PrimaryButton href="/#/chapters/1" text="Start Reading" />
+                <PrimaryButton onClick={handleStartReading} text="Start Reading" />
             </Stack>
         </Stack>
     )
